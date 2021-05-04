0bff1a578ab81c395b67782d0b1223da33138f09
sujith <sujith0709@gmail.com>
2021-05-04 16:44:02 -0400

    added login, register endpoints, and made a sepeare endpoint fo insurer search claim, uses jwt, bcrypt, and cookie parser, also changed cors

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,9 @@ import { env } from "process";
 import { MongoClient } from "mongodb";
 import { v4 as UUIDv4 } from "uuid";
 import { PLACE_AUTOCOMPLETE, STATIC_MAPS } from "./external";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
 
 const port = env["PORT"] ?? "8080";
 const host = env["HOST"] ?? "127.0.0.1";
@@ -31,9 +34,18 @@ const mongo = MongoClient.connect(dbHost)
   .then((client) => client.db("hartford"))
   .then((client) => client.collection("claims"));
 
-app.use(express.json());
-app.use(cors());
+const loginCollection = MongoClient.connect(dbHost)
+  .then((client) => client.db("hartford"))
+  .then((client) => client.collection("login_credentials"));
 
+app.use(express.json());
+app.use(
+  cors({
+    origin: [`http://localhost:3000`, `http://localhost:8000`],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
 /**
  * This endpoint retrieves a document from the database by its UUID.
  * This endpoint expects a claim ID in the form of a UUIDv4.  It
@@ -84,22 +96,52 @@ app.post("/claims", (req, res) => {
     .then(() => res.end(JSON.stringify(response)));
 });
 
+app.get("/claims", (req, res) => {
+  const query = {
+    firstName: req.query["firstName"] ?? "",
+    lastName: req.query["lastName"] ?? "",
+    policyNumber: req.query["policyNumber"] ?? "",
+  };
+  console.log(query);
+  if (
+    query.firstName == "" ||
+    query.lastName == "" ||
+    query.policyNumber == ""
+  ) {
+    res.status(400).send("required params missing ");
+  } else {
+    mongo
+      .then((db) => db.find(query))
+      .then((db) => db.toArray())
+      .then((db) => JSON.stringify(db))
+      .then((db) => res.end(db));
+  }
+});
+//this endpoint is to search through the databse, all the params are required
+
 /**
  * This endpoint is used to search through the database
  * for any claims that match the given params
  */
-app.get("/claims", (req, res) => {
-  const query = {
-    firstName: req.query["firstName"] ?? /.*/,
-    lastName: req.query["lastName"] ?? /.*/,
-    policyNumber: req.query["policyNumber"] ?? /.*/,
-  };
+app.get("/insurer/claims", (req, res) => {
+  const cookie = req.cookies["jwt"];
+  jwt.verify(cookie, "secret", (err: any, decoded: any) => {
+    if (err) {
+      res.status(401).send("unauthenticated");
+    } else {
+      const query = {
+        firstName: req.query["firstName"] ?? /.*/,
+        lastName: req.query["lastName"] ?? /.*/,
+        policyNumber: req.query["policyNumber"] ?? /.*/,
+      };
 
-  mongo
-    .then((db) => db.find(query))
-    .then((db) => db.toArray())
-    .then((db) => JSON.stringify(db))
-    .then((db) => res.end(db));
+      mongo
+        .then((db) => db.find(query))
+        .then((db) => db.toArray())
+        .then((db) => JSON.stringify(db))
+        .then((db) => res.end(db));
+    }
+  });
 });
 
 /**
@@ -128,4 +170,54 @@ app.get("/address/:input", (req, res) => {
   );
 });
 
+app.post("/login", (req, res) => {
+  loginCollection
+    .then((db) => db.findOne({ userName: req.body.user_name }))
+    .then((y) => {
+      console.log(y);
+      if (y == null) {
+        res.status(400).send("no user found");
+      } else {
+        bcrypt.compare(req.body.password, y.password, (err, same) => {
+          if (err) {
+            throw err;
+          }
+          if (same) {
+            const token = jwt.sign(y._id, "secret");
+            res.cookie("jwt", token, {
+              httpOnly: true,
+              //one day
+              maxAge: 1000 * 60 * 60 * 24,
+            });
+            res.setHeader("access-control-expose-headers", "Set-Cookie");
+            res.send(token);
+          } else {
+            res.send("don't match");
+          }
+        });
+      }
+    });
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body["password"], 10);
+    const document = {
+      _id: UUIDv4(),
+      userName: req.body["user_name"],
+      password: hashedPassword,
+      email: req.body["email"],
+    };
+    loginCollection
+      .then((db) => db.insertOne(document))
+      .then(() => res.status(200).send());
+  } catch {
+    res.status(500).send();
+  }
+});
+
+app.post("/logout", (req, res) => {
+  res.cookie("jwt", "", { maxAge: 0 });
+  res.send("logged out");
+});
 app.listen(parseInt(port), host);
